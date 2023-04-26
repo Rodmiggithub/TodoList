0f42bbbd6efea3a5fb30c2c4df1b409aa5a90b06
Rodrigo <rodrigomperezb@gmail.com>
2023-04-26 11:58:08 -0400

    Primer commit

---
+++ src/todos/app.js
@@ -0,0 +1,96 @@
+import html from './app.html?raw';
+import todoStore, { Filters } from '../store/todo.store';
+import { renderTodos, renderPending } from './use-cases';
+
+
+const elementIds = {
+    clearCompletedButton: '.clear-completed',
+    TodoList: '.todo-list',
+    newTodoInput: '#new-todo-input',
+    TodoFilters: '.filtro',
+    PendingCountLabel: '#pending-count',
+}
+/**
+ * 
+ * @param {String} elementId 
+ */
+
+export const App = (elementId) => {
+
+    const displayTodos = () => {
+
+        const todos = todoStore.getTodos( todoStore.getCurrentFilter());
+        renderTodos(elementIds.TodoList, todos);
+        updatePendingCount();
+        
+    }
+    const updatePendingCount = () => {
+        renderPending(elementIds.PendingCountLabel);
+    }
+
+    (()=>{
+
+        const app = document.createElement('div');
+        app.innerHTML = html;
+        document.querySelector(elementId).append(app);
+        displayTodos();
+    })();
+
+
+
+    //Referencias HTML
+
+    const newDescriptionInput = document.querySelector(elementIds.newTodoInput);
+    const todoListUL = document.querySelector(elementIds.TodoList);
+    const clearCompletedButton = document.querySelector(elementIds.clearCompletedButton);
+    const filterLIs = document.querySelectorAll(elementIds.TodoFilters);
+    //Listeners
+    newDescriptionInput.addEventListener('keyup', (event) =>{
+        if(event.keyCode !== 13) return;
+        if(event.target.value.trim().length === 0) return;
+
+        todoStore.addTodo(event.target.value);
+        displayTodos();
+        event.target.value = '';
+    });
+
+    todoListUL.addEventListener('click', (event)=>{
+        const element = event.target.closest('[data-id]');
+        todoStore.toggleTodo(element.getAttribute('data-id'));
+        displayTodos();
+    });
+    todoListUL.addEventListener('click', (event)=>{
+        const isDestroyElement = event.target.className === 'destroy';
+        const element = event.target.closest('[data-id]');
+        if(!element  || !isDestroyElement) return;
+        todoStore.deleteTodo(element.getAttribute('data-id'));
+        displayTodos();
+    });
+
+    clearCompletedButton.addEventListener('click', ()=>{
+
+        todoStore.deleteCompleted();
+        displayTodos();
+    });
+    filterLIs.forEach(element => {
+
+        element.addEventListener('click',(element)=>{
+            filterLIs.forEach(el => el.classList.remove('selected'));
+            element.target.classList.add('selected');
+
+            switch(element.target.text){
+                case 'Todos':
+                    todoStore.setFilter(Filters.All)
+                    break;
+                case 'Pendientes':
+                        todoStore.setFilter(Filters.Pending)
+                    break;
+                case 'Completados':
+                        todoStore.setFilter(Filters.Completed)
+                    break;
+            }
+            displayTodos();
+        });
+    });
+
+}
